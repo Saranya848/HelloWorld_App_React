bdb643be16002552048bc040289472d71edaa8cb
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-06-09 11:14:24 +0530

    Clicking on Logo and displays bridgelabz website

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 import React from "react";
 
 class App extends React.Component{
+  url="https://www.bridgelabz.com";
   constructor(){
     super();
     this.state = { message: 'Hello from Bridgelabz'}
+  }
+
+  onClick = (Sevent) =>{
+   console.log("Save button", Sevent);
+   window.open(this.url, "_blank");
   }
   render(){
     return(
@@ -13,7 +19,7 @@ class App extends React.Component{
       <header className="App-header">
                    {this.state.message}
       </header>
-      <img className="image" src={logo} alt="Bridgelabz Logo"/>
+      <img className="image" onClick={this.onClick} src={logo} alt="Bridgelabz Logo"/>
     </div>
   )
 }
